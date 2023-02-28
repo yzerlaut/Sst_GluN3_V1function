f4ff464399d1b79d852a409acd91306461d7929c
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2023-02-28 08:38:31 +0100

    update summary

--- notebooks/scripts/Summary.py
+++ notebooks/scripts/Summary.py
@@ -35,12 +35,12 @@ stat_test_props = dict(interval_pre=[-1.5,0],
                                                                                                                                                                
 response_significance_threshold = 0.01                                                                                                                         
 
-SUMMARY = {'WT':{}, 'GLUN3':{}, 'NR1':{}}
+SUMMARY = {'WT':{}, 'GluN3':{}, 'NR1':{}}
 
-SUMMARY['GLUN3']['FILES'] = [\
-               '/home/yann.zerlaut/CURATED/SST-Glun3KO-January-2023/2023_01_12-18-47-40.nwb',
-               '/home/yann.zerlaut/CURATED/SST-Glun3KO-January-2023/2023_01_12-21-01-10.nwb',
-               '/home/yann.zerlaut/CURATED/SST-Glun3KO-January-2023/2023_01_12-21-51-21.nwb']
+SUMMARY['GluN3']['FILES'] = [\
+               '/home/yann.zerlaut/CURATED/SST-GluN3KO-January-2023/2023_01_12-18-47-40.nwb',
+               '/home/yann.zerlaut/CURATED/SST-GluN3KO-January-2023/2023_01_12-21-01-10.nwb',
+               '/home/yann.zerlaut/CURATED/SST-GluN3KO-January-2023/2023_01_12-21-51-21.nwb']
 
 
 SUMMARY['NR1']['FILES'] = [\
@@ -58,7 +58,7 @@ SUMMARY['WT']['FILES'] = [\
             '/home/yann.zerlaut/CURATED/SST-GluN3KO-February-2023/2023_02_15-15-10-04.nwb',
             '/home/yann.zerlaut/CURATED/SST-GluN3KO-February-2023/2023_02_15-15-48-06.nwb']
 
-for key in ['WT', 'GLUN3', 'NR1']:
+for key in ['WT', 'GluN3', 'NR1']:
 
     SUMMARY[key]['RESPONSES'], SUMMARY[key]['FRAC_RESP'] = [], []
 
@@ -68,6 +68,99 @@ for key in ['WT', 'GLUN3', 'NR1']:
         responses, frac_resp, shifted_angle = compute_tuning_response_per_cells(data,
                                                                                 verbose=False)
         SUMMARY[key]['RESPONSES'].append(responses)
-        WT_FRAC_RESP.append(frac_resp)
+        SUMMARY[key]['FRAC_RESP'].append(frac_resp)
+
+# %%
+from physion.analysis.behavior import population_analysis
+fig, ax = population_analysis(np.concatenate([SUMMARY[key]['FILES'] for key in ['WT', 'GluN3', 'NR1']]),
+                              running_speed_threshold=0.1,
+                            ax=None)
+
+
+# %%
+def plot_tunning_summary(shifted_angle, frac_resp, responses):
+    """
+    """
+    fig, AX = pt.plt.subplots(1, 3, figsize=(6,1.5))
+    pt.plt.subplots_adjust(wspace=0.8, top=0.7, bottom=0.3)
+
+    RESPONSES = [np.mean(responses, axis=0) for responses in responses]
+    
+    # raw
+    pt.plot(shifted_angle, np.mean(RESPONSES, axis=0),
+            sy=np.std(RESPONSES, axis=0), ax=AX[0])
+    
+    
+    AX[0].set_ylabel('$\Delta$F/F')
+    AX[0].set_title('raw resp.')
+
+    for ax in AX[:2]:
+        ax.set_xlabel('angle ($^o$)')
+        ax.annotate('N=%i sessions'%len(responses), (1,1), fontsize=6,
+                    va='top', ha='right', xycoords='axes fraction')
+
+    # peak normalized
+    N_RESP = [resp/resp[1] for resp in RESPONSES]
+    pt.plot(shifted_angle, np.mean(N_RESP, axis=0),
+            sy=np.std(N_RESP, axis=0), ax=AX[1])
+
+    AX[1].set_yticks([0, 0.5, 1])
+    AX[1].set_ylabel('n. $\Delta$F/F')
+    AX[1].set_title('peak normalized')
+
+    pt.pie([np.mean(frac_resp), 1-np.mean(frac_resp)],
+           pie_labels=['%.1f%%' % (100.*np.mean(frac_resp)),
+                       '     %.1f%%' % (100.*(1-np.mean(frac_resp)))],
+           COLORS=[pt.plt.cm.tab10(2), pt.plt.cm.tab10(1)], ax=AX[2])
+    
+    NTOTs = [int(len(resp)/fr) for fr, resp in zip(frac_resp, responses)]
+    Ns = [len(resp) for resp in responses]
+    AX[2].annotate('responsive ROIS :\nN=%i sessions\n n= %i$\pm$%i / %i$\pm$%i ROIs  ' % (len(responses),
+                                                                        np.mean(Ns), np.std(Ns),
+                                                                        np.mean(NTOTs), np.std(NTOTs)),
+                   (0.5, 0), va='top', ha='center',
+                   xycoords='axes fraction')
+    #ge.save_on_desktop(fig, 'fig.png', dpi=300)
+    return fig, AX
+ 
+for key in ['WT', 'GluN3', 'NR1']:
+
+    fig, AX = plot_tunning_summary(shifted_angle,
+                                   SUMMARY[key]['FRAC_RESP'], SUMMARY[key]['RESPONSES'])
+    AX[2].set_title(key+' mice');
+    fig.savefig(os.path.join(os.path.expanduser('~'), 'Desktop', key+'.svg'))
+
+
+# %%
+fig, AX = pt.plt.subplots(3, 1, figsize=(1.8,4))
+pt.plt.subplots_adjust(hspace=0.7, top=0.95, bottom=0.2, left=0.33)
+
+for i, key, color in zip(range(3), ['WT', 'GluN3', 'NR1'], ['k', 'tab:blue', 'tab:green']):
+    
+    RESPONSES = [np.mean(responses, axis=0) for responses in SUMMARY[key]['RESPONSES']]
+    pt.plot(shifted_angle, np.mean(RESPONSES, axis=0),
+            0*np.std(RESPONSES, axis=0),
+            ax=AX[0], color=color)
+    
+    N_RESP = [resp/resp[1] for resp in RESPONSES]
+    pt.plot(shifted_angle, np.mean(N_RESP, axis=0),
+            sy=0*np.std(N_RESP, axis=0), ax=AX[1], color=color)
+    
+    AX[2].bar([i],
+              [100*np.mean(SUMMARY[key]['FRAC_RESP'])],
+              yerr=[100*np.std(SUMMARY[key]['FRAC_RESP'])],
+              color=color)
+
+AX[0].set_ylabel('$\Delta$F/F')
+AX[0].set_xlabel('angle ($^o$)')
+AX[1].set_ylabel('n. $\Delta$F/F')
+AX[1].set_xlabel('angle ($^o$)')
+
+AX[2].set_ylabel('fraction (%)\nresponsive')
+AX[2].set_xticks([0,1,2])
+AX[2].set_xticklabels(['WT', 'GluN3', 'NR1'], rotation=70)
+AX[2].set_xlim([-2,4])
+
+fig.savefig(os.path.join(os.path.expanduser('~'), 'Desktop', 'final.svg'))
 
 # %%
