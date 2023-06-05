7acc3379c01c9686e5f0a43d9061111e8968300e
Yann Zerlaut <yann.zerlaut@gmail.com>
2023-06-05 17:52:43 +0200

    adding data folder

--- notebooks/Size-Tuning-SST.py
+++ notebooks/Size-Tuning-SST.py
@@ -35,95 +35,151 @@ warnings.filterwarnings("ignore") # disable the UserWarning from pynwb (arrays a
 DATASET = scan_folder_for_NWBfiles(folder,
                                    verbose=False)
 
+
 # %%
 # -------------------------------------------------- #
 # ----    Pick the session datafiles and sort ------ #
 # ----      them according to genotype ------------- #
 # -------------------------------------------------- #
 
-SUMMARY = {'WT':{'FILES':[], 'subjects':[]}, 
-           'GluN1':{'FILES':[], 'subjects':[]}, 
-           'GluN3':{'FILES':[], 'subjects':[]}}
-
-for i, protocols in enumerate(DATASET['protocols']):
+def init_summary(DATASET):
     
-    # select the sessions
-    if ('size-tuning' in protocols[0]):
-        
-        # sort the sessions according to the mouse genotype
-        if ('NR1' in DATASET['subjects'][i]) or ('GluN1' in DATASET['subjects'][i]):
-            SUMMARY['GluN1']['FILES'].append(DATASET['files'][i])
-            SUMMARY['GluN1']['subjects'].append(DATASET['subjects'][i])
-        elif 'GluN3' in DATASET['subjects'][i]:
-            SUMMARY['GluN3']['FILES'].append(DATASET['files'][i])
-            SUMMARY['GluN3']['subjects'].append(DATASET['subjects'][i])
-        else:
-            SUMMARY['WT']['FILES'].append(DATASET['files'][i])
-            SUMMARY['WT']['subjects'].append(DATASET['subjects'][i])
+    SUMMARY = {'WT':{'FILES':[], 'subjects':[]}, 
+               'GluN1':{'FILES':[], 'subjects':[]}, 
+               'GluN3':{'FILES':[], 'subjects':[]}}
+
+    for i, protocols in enumerate(DATASET['protocols']):
+
+        # select the sessions
+        if ('size-tuning' in protocols[0]):
+
+            # sort the sessions according to the mouse genotype
+            if ('NR1' in DATASET['subjects'][i]) or ('GluN1' in DATASET['subjects'][i]):
+                SUMMARY['GluN1']['FILES'].append(DATASET['files'][i])
+                SUMMARY['GluN1']['subjects'].append(DATASET['subjects'][i])
+            elif 'GluN3' in DATASET['subjects'][i]:
+                SUMMARY['GluN3']['FILES'].append(DATASET['files'][i])
+                SUMMARY['GluN3']['subjects'].append(DATASET['subjects'][i])
+            else:
+                SUMMARY['WT']['FILES'].append(DATASET['files'][i])
+                SUMMARY['WT']['subjects'].append(DATASET['subjects'][i])
+                
+    return SUMMARY
+
 
 # %%
 from physion.analysis.protocols.size_tuning import center_and_compute_size_tuning
 
-for key in ['WT', 'GluN1']:
+def run_dataset_analysis(DATASET,
+                         quantity='dFoF',
+                         neuropil_correction_factor = 1.,
+                         method_for_F0 = 'sliding_percentile',
+                         percentile=5., # percent
+                         sliding_window = 180.0, # seconds
+                         Nmax=999, # max datafiles (for debugging)
+                         verbose=True):
 
-    for k in ['RESPONSES', 'CENTERED_ROIS', 'PREF_ANGLES']:
-        SUMMARY[key][k] = [] 
+    SUMMARY = init_summary(DATASET)
+    
+    SUMMARY['quantity'] = quantity
+    SUMMARY['quantity_args'] = dict(method_for_F0=method_for_F0,
+                                    percentile=percentile,
+                                    sliding_window=sliding_window,
+                                    neuropil_correction_factor=neuropil_correction_factor)
+    
+    for key in ['WT', 'GluN1']:
 
-    for f in SUMMARY[key]['FILES']:
-        
-        print('analyzing "%s" [...] ' % f)
-        data = Data(f, verbose=False)
-        data.build_dFoF()
-        print('-->', data.vNrois)
-        radii, size_resps, rois, pref_angles = center_and_compute_size_tuning(data, 
-                                                                              with_rois_and_angles=True,
-                                                                              verbose=False)
-        
-        if len(size_resps)>0:
-            for k, q in zip(['RESPONSES', 'CENTERED_ROIS', 'PREF_ANGLES'],
-                            [size_resps, rois, pref_angles]):
-                SUMMARY[key][k].append(q)
-        
-        if len(radii)>0:
-            SUMMARY['radii'] = radii
+        for k in ['RESPONSES', 'CENTERED_ROIS', 'PREF_ANGLES']:
+            SUMMARY[key][k] = [] 
+
+        for f in SUMMARY[key]['FILES'][:Nmax]:
+
+            print('analyzing "%s" [...] ' % f)
+            data = Data(f, verbose=False)
+
+            if quantity=='dFoF':
+                data.build_dFoF(method_for_F0=method_for_F0,
+                                percentile=percentile,
+                                sliding_window=sliding_window,
+                                neuropil_correction_factor=neuropil_correction_factor,
+                                verbose=False)
+            elif quantity=='rawFluo':
+                data.build_rawFluo(verbose=verbose)
+            elif quantity=='neuropil':
+                data.build_neuropil(verbose=verbose)            
+            else:
+                print('quantity not recognized !!')
+
+            #print('-->', data.vNrois)
+            radii, size_resps, rois, pref_angles = center_and_compute_size_tuning(data,
+                                                                                  imaging_quantity=quantity,
+                                                                                  with_rois_and_angles=True,
+                                                                                  verbose=False)
+            if len(size_resps)>0:
+                for k, q in zip(['RESPONSES', 'CENTERED_ROIS', 'PREF_ANGLES'],
+                                [size_resps, rois, pref_angles]):
+                    SUMMARY[key][k].append(q)
+
+            if len(radii)>0:
+                SUMMARY['radii'] = radii
+                
+    return SUMMARY
+
+SUMMARY = run_dataset_analysis(DATASET, quantity='neuropil', Nmax=2, verbose=False)
 
 # %%
-fig, AX = pt.plt.subplots(1, 3, figsize=(7,1.5))
-AX[0].annotate('average\nover\nsessions', (-0.8, 0.5), va='center', ha='center', xycoords='axes fraction')
-plt.subplots_adjust(wspace=0.5)
+SUMMARY
 
-center_index = 2
 
-for i, key, color in zip(range(3), ['WT', 'GluN1'], ['k', 'tab:blue']):
+# %%
+
+def plot_summary(SUMMARY,
+                 average_by='sessions'):
     
-    if (len(SUMMARY[key]['RESPONSES'])>0) and (len(SUMMARY[key]['RESPONSES'][0])>0):
-        
-        resp = np.array([np.mean(r, axis=0) for r in SUMMARY[key]['RESPONSES']])
-        
-        AX[0].plot(SUMMARY['radii'][1:], np.mean(resp, axis=0)[1:], 'o', ms=2, color=color)
-        pt.plot(SUMMARY['radii'], np.mean(resp, axis=0), sy=np.std(resp, axis=0),
-                ax=AX[0], color=color)
-        
-        center_norm_resp = np.array([r/rn for r, rn in zip(resp, resp[:,center_index])])
-        pt.plot(SUMMARY['radii'], np.mean(center_norm_resp, axis=0), sy=np.std(center_norm_resp, axis=0),
-                ax=AX[1], color=color)
+    fig, AX = pt.plt.subplots(1, 3, figsize=(7,1.5))
+    AX[0].annotate('average\nover\n%s' % average_by,
+                   (-0.8, 0.5), va='center', ha='center', xycoords='axes fraction')
+    plt.subplots_adjust(wspace=0.5)
 
-        ff_norm_resp = np.array([r/rn for r, rn in zip(resp, resp[:,-1])])
-        pt.plot(SUMMARY['radii'], np.mean(ff_norm_resp, axis=0), sy=np.std(ff_norm_resp, axis=0),
-                ax=AX[2], color=color)
-        
-    AX[-1].annotate(i*'\n'+'%s, N=%i sessions' % (key, len(resp)), (1,1),
-                    va='top', color=color, xycoords='axes fraction')
+    center_index = 2
 
-AX[1].plot([SUMMARY['radii'][center_index], SUMMARY['radii'][center_index], 0],
-           [0,1,1], 'k:', lw=0.5)
-AX[2].plot([SUMMARY['radii'][-1], SUMMARY['radii'][-1], 0], [0,1,1], 'k:', lw=0.5)
+    for i, key, color in zip(range(3), ['WT', 'GluN1'], ['k', 'tab:blue']):
 
-for ax, ylabel in zip(AX,
-                     ['$\delta$ $\Delta$F/F', 'center norm. $\delta$ $\Delta$F/F', 'F.F. norm. $\delta$ $\Delta$F/F']):
-    pt.set_plot(ax, xlabel='size ($^o$)', ylabel=ylabel)    
+        if (len(SUMMARY[key]['RESPONSES'])>0) and (len(SUMMARY[key]['RESPONSES'][0])>0):
 
+            resp = np.array([np.mean(r, axis=0) for r in SUMMARY[key]['RESPONSES']])
+
+            AX[0].plot(SUMMARY['radii'][1:], np.mean(resp, axis=0)[1:], 'o', ms=2, color=color)
+            pt.plot(SUMMARY['radii'], np.mean(resp, axis=0), sy=np.std(resp, axis=0),
+                    ax=AX[0], color=color)
+
+            center_norm_resp = np.array([r/rn for r, rn in zip(resp, resp[:,center_index])])
+            pt.plot(SUMMARY['radii'], np.mean(center_norm_resp, axis=0), sy=np.std(center_norm_resp, axis=0),
+                    ax=AX[1], color=color)
+
+            ff_norm_resp = np.array([r/rn for r, rn in zip(resp, resp[:,-1])])
+            pt.plot(SUMMARY['radii'], np.mean(ff_norm_resp, axis=0), sy=np.std(ff_norm_resp, axis=0),
+                    ax=AX[2], color=color)
+
+        AX[-1].annotate(i*'\n'+'%s, N=%i sessions' % (key, len(resp)), (1,1),
+                        va='top', color=color, xycoords='axes fraction')
+
+    AX[1].plot([SUMMARY['radii'][center_index], SUMMARY['radii'][center_index], 0],
+               [0,1,1], 'k:', lw=0.5)
+    AX[2].plot([SUMMARY['radii'][-1], SUMMARY['radii'][-1], 0], [0,1,1], 'k:', lw=0.5)
+
+    for ax, ylabel in zip(AX,
+                         ['$\delta$ %s', 'center norm. $\delta$ %s', 'F.F. norm. $\delta$ %s']):
+        pt.set_plot(ax, xlabel='size ($^o$)',
+                    ylabel=ylabel % SUMMARY['quantity'].replace('dFoF', '$\Delta$F/F'))
+
+    return fig
+
+fig = plot_summary(SUMMARY)
 #fig.savefig(os.path.join(os.path.expanduser('~'), 'Desktop', 'final.svg'))
+
+# %%
+fig = plot_summary(SUMMARY)
 
 # %%
 from physion.analysis.protocols.size_tuning import center_and_compute_size_tuning
